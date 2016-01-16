b66c1ebf31e2f42389739cf3349b4b32d0923ecf
Joe Kim <josephkim0224@gmail.com>
2016-01-16 14:04:38 -0500

    Fixed issues since last commit

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe "UserPages", type: :request do
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Test User"
-        fill_in "Email",        with: "tester@exampler.com"
+        fill_in "Email",        with: "tester@example.com"
         fill_in "Password",     with: "secret"
         fill_in "Confirmation", with: "secret"
       end
@@ -36,11 +36,26 @@ RSpec.describe "UserPages", type: :request do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'tester@example.com') }
+
+        it { is_expected.to have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { is_expected.to have_title(user.name) }
+      end
     end
     
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { is_expected.to have_content('error') }
+        it { is_expected.to have_title('Sign up') }
       end
     end
   end

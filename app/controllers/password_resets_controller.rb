43e16e9c5e81a0c614025e765bd56eb2035852cb
Joe Kim <josephkim0224@gmail.com>
2016-01-19 22:56:15 -0500

    Activation and Password reset fix 2

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -37,6 +37,12 @@ class PasswordResetsController < ApplicationController
 
   private
 
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Before filters
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
